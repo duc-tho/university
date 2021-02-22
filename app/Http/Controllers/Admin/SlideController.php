88c4a9259e3ce3ee27e1da635e7d4fd66a21bfcc
toanvo97 <73413706+toanvo97@users.noreply.github.com>
2021-02-22 10:22:10 +0700

    ểdit

--- app/Http/Controllers/Admin/SlideController.php
+++ app/Http/Controllers/Admin/SlideController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Slide;
 
 class SlideController extends Controller
 {
@@ -12,5 +13,13 @@ class SlideController extends Controller
     {
         # code...
         return view('server.pages.slide.index');
+    }
+
+    public function getSlide(){
+        return view('server.pages.slide.addslide');
+    }
+
+    public function postSlide(){
+
     }
 }
